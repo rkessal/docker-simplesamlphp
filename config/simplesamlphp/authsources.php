d3dd4263826c2f9625635ff7180ddde860db2c83
rkessal <kessal.rayhan25@gmail.com>
2025-04-10 15:32:01 +0100

    local sso

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,10 +8,19 @@ $config = array(
     'example-userpass' => array(
         'exampleauth:UserPass',
         'user1:password' => array(
-            'email' => 'user1@example.com',
+            'mail' => 'adiogo@test.fr',
+            'login_ad' => 'ARDG2',
+            'IdpTargetId' => 'ARDG',
+            'name' => 'Andre',
+            'first_name' => 'test andre',
+            'last_name' => 'test yes',
         ),
         'user2:password' => array(
             'email' => 'user2@example.com',
+            'IdpTargetId' => '99513',
+            'name' => 'Jacqueline SANTHIOGU',
+            'first_name' => 'Jacqueline',
+            'last_name' => 'SANTHIOGU',
         ),
     ),
 
